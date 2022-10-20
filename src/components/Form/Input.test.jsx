bd58d2ea18da70ce3daf71f65abc2eac15d67293
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-19 20:37:46 -0500

    test for correct type of Input

--- src/components/Form/Input.test.jsx
+++ src/components/Form/Input.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Input from "./Input";
 
 describe("Input", () => {
-  it("renders with the correct label, placeholder, and id", () => {
+  it("renders with the correct label, placeholder, and id and is of type text", () => {
     render(<Input label="label" />);
 
     // Act
@@ -10,5 +10,16 @@ describe("Input", () => {
 
     // Assert
     expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("type", "text");
+  });
+
+  it("renders with the correct type", () => {
+    render(<Input label="label" type="email" />);
+
+    // Act
+    const input = screen.getByLabelText("label");
+
+    // Assert
+    expect(input).toHaveAttribute("type", "email");
   });
 });
