c7947e59c7f38cde7734e46cb09b7f665d97c847
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-19 20:22:53 -0500

    move input map to inside of Form component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,18 +3,22 @@ import Button from "../Button";
 import Input from "./Input";
 
 // function to render array of inputs
+/*
 function renderInputs(fields) {
   return fields.map((inputNum) => (
     <Input key={inputNum.id} label={inputNum.label} type={inputNum.type} />
   ));
 }
+*/
 
 // Form function
 // accepts and destructures props for input fields and the button
 export default function Form({ inputs, button }) {
   return (
     <form className="mx-auto mt-2 max-w-xs">
-      {renderInputs(inputs)}
+      {inputs.map((inputNum) => (
+        <Input key={inputNum.id} label={inputNum.label} type={inputNum.type} />
+      ))}
       {Button(button)}
     </form>
   );
