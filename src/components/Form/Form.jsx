5d5424bd9d9cf2558f23bf7b390fd95a9c8fac78
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-24 21:40:36 -0500

    add submit function for form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,6 +14,13 @@ function renderInputs(fields) {
 // Form function
 // accepts and destructures props for input fields and the button
 export default function Form({ inputs, button }) {
+  // submit function for the form
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted"); // just to see if working
+    // should probably add something to get input values
+  }
+
   return (
     <form className="mx-auto mt-2 max-w-xs">
       {inputs.map((inputNum) => (
