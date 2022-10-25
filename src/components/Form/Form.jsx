2ce20adaa8e6c647319a327fa5c195d3c39415d6
Jacob Vielweber <jnvielweber@gmail.com>
2022-10-25 00:52:19 -0500

    handle onSubmit in Form component without useRef

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -14,8 +14,19 @@ function renderInputs(fields) {
 // Form function
 // accepts and destructures props for input fields and the button
 export default function Form({ inputs, button }) {
+  // useRef might be awkward as the inputs are seperate components we call
+  // however a form knows about its own inputs so we don't need them
+  function noRefHandleSubmit(event) {
+    event.preventDefault();
+    const thisForm = event.target;
+    inputs.forEach((crntInput) =>
+      console.log(crntInput.label, thisForm[crntInput.label].value)
+    );
+    // I can barely believe this works
+  }
+
   return (
-    <form className="mx-auto mt-2 max-w-xs">
+    <form className="mx-auto mt-2 max-w-xs" onSubmit={noRefHandleSubmit}>
       {inputs.map((inputNum) => (
         <Input key={inputNum.id} label={inputNum.label} type={inputNum.type} />
       ))}
